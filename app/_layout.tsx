e89d9c10c0b89e323f974c9ee155018ef43f22c7
MuxN4 <stev3npy@gmail.com>
2026-04-04 14:48:59 +0300

    feat: font implementation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,25 @@
 import "@/global.css";
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    "sans-regular": require("../assets/fonts/Urbanist-Regular.ttf"),
+    "sans-medium": require("../assets/fonts/Urbanist-Medium.ttf"),
+    "sans-bold": require("../assets/fonts/Urbanist-Bold.ttf"),
+    "sans-semibold": require("../assets/fonts/Urbanist-SemiBold.ttf"),
+    "sans-extrabold": require("../assets/fonts/Urbanist-ExtraBold.ttf"),
+    "sans-light": require("../assets/fonts/Urbanist-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
