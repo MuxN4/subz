a13c7b8d85d86276892fd73d8f131934b281c3fa
MuxN4 <stev3npy@gmail.com>
2026-04-05 17:11:20 +0300

    feat: add Clerk authentication setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,18 @@
 import "@/global.css";
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 
 SplashScreen.preventAutoHideAsync();
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
 
 export default function RootLayout() {
   const [fontsLoaded, error] = useFonts({
@@ -17,14 +25,18 @@ export default function RootLayout() {
   });
 
   useEffect(() => {
-    if (error) throw error;
-
     if (fontsLoaded) {
       SplashScreen.hideAsync();
     }
-  }, [fontsLoaded, error]);
+  }, [fontsLoaded]);
+
+  if (error) console.error(error);
 
   if (!fontsLoaded) return null;
 
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </ClerkProvider>
+  );
 }
